cde00a20f6a85b7f1cd39e3b2179c5a166954509
Dinushi Dhananjani <dinushidhananjani@gmail.com>
2021-10-31 13:24:23 +0530

    python

--- Blockchain implementation/blockchain_main.py
+++ Blockchain implementation/blockchain_main.py
@@ -241,3 +241,21 @@ def replace_chain():
  
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
+
+# Python Program to find the area of triangle
+
+a = 5
+b = 6
+c = 7
+
+# Uncomment below to take inputs from the user
+# a = float(input('Enter first side: '))
+# b = float(input('Enter second side: '))
+# c = float(input('Enter third side: '))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+# calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %
